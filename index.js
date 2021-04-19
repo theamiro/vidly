019c459042b754dfc31444e6993e9f6d3bf278e3
Michael Amiro <odindo.michael@gmail.com>
2021-04-20 00:23:56 +0300

    Fixed Morgan Logging of requests

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const debug = require("debug")("app:startup")
 const helmet = require("helmet")
 const app = express()
 
+if (process.env.ENV === "development") {
+	app.use(morgan("dev"))
+	debug("Running in developement")
+}
 require("./app/logger")
 require("./app/db")
 require("./app/routes")(app)
@@ -14,11 +18,6 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("./public"))
 app.use(helmet())
-
-if (process.env.ENV === "development") {
-	app.use(morgan("tiny"))
-	debug("Running in developement")
-}
 
 const port = process.env.PORT || 9000
 app.listen(port, () => debug("Listening on port " + port))
